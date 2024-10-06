fd21c585e8ac8035a2c57eca673a693fb62db640
vanek021 <vanekmenyakin@gmail.com>
2024-10-06 10:58:27 +0500

    + Новая SVG скелета, но не доделанная

---
+++ YATM.Models/Enums/JointType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YATM.Models.Enums
+{
+    public enum JointType
+    {
+        RightEar = 0,
+        LeftEar = 1,
+        Head = 2,
+        Neck = 3,
+    }
+}
